ce111be23e09ece426d7f0024a29701139f47462
LegendaryGuard <rootuser999@gmail.com>
2022-08-24 00:40:36 +0200

    Fix pk3 compression

--- make_pak9.bat
+++ make_pak9.bat
@@ -1,7 +1,7 @@
 set PATH=C:\quake3\tools\bin;%PATH%
 
-REM cd binaries
-tools\bin\7za.exe a -tzip pak9.pk3 binaries\vm
-move pak9.pk3 binaries
+cd binaries
+..\tools\bin\7za.exe a -tzip pak9.pk3 vm
 
+cd ..
 :quit
