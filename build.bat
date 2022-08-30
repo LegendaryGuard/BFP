57f6bf8f9339af1df75355a62050f1ce6ee21c82
Mjuksel <mportma@gmail.com>
2022-08-30 07:25:32 +0200

    created PM_CheckFlying, removed fly cmd, speed 400

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 set PATH=%~dp0\tools\bin;%PATH%
+set q3path=C:\Games\quake3\Xx
 
 cd source\game
 CALL game.bat
@@ -13,10 +14,12 @@ CALL q3_ui.bat
 cd ..\..\
 
 cd binaries
-..\tools\bin\7za.exe a -tzip ..\pak9.pk3 vm
+..\tools\bin\7za.exe a -tzip %q3path%\bfpq3\pak0.pk3 vm
 cd ..
 
 rd binaries /s /q
 rd intermediate /q /s
+
+%q3path%\quake3e.x64 +set fs_game bfpq3 +devmap testmap
 
 :quit
