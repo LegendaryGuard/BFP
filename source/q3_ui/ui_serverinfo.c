04c76d5041684e3a98350494f4e7609ecb0a781a
LegendaryGuard <rootuser999@gmail.com>
2023-09-02 01:18:46 +0200

    q3_ui: Apply changes in SERVER INFO menu

--- source/q3_ui/ui_serverinfo.c
+++ source/q3_ui/ui_serverinfo.c
@@ -22,6 +22,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
 #include "ui_local.h"
 
+#define ART_MENUBG			"menu/art/menubg"		// BFP - Menu background
+#define ART_BARLOG			"menu/art/cap_barlog"	// BFP - barlog
 #define SERVERINFO_FRAMEL	"menu/art/frame2_l"
 #define SERVERINFO_FRAMER	"menu/art/frame1_r"
 #define SERVERINFO_BACK0	"menu/art/back_0"
@@ -29,8 +31,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 static char* serverinfo_artlist[] =
 {
-	SERVERINFO_FRAMEL,	
-	SERVERINFO_FRAMER,
 	SERVERINFO_BACK0,
 	SERVERINFO_BACK1,
 	NULL
@@ -43,8 +43,8 @@ typedef struct
 {
 	menuframework_s	menu;
 	menutext_s		banner;
-	menubitmap_s	framel;
-	menubitmap_s	framer;
+	menubitmap_s	menubg; // BFP - Menu background
+	menubitmap_s	barlog; // BFP - barlog
 	menubitmap_s	back;
 	menutext_s		add;
 	char			info[MAX_INFO_STRING];
@@ -140,7 +140,7 @@ static void ServerInfo_MenuDraw( void )
 
 		Q_strcat( key, MAX_INFO_KEY, ":" ); 
 
-		UI_DrawString(SCREEN_WIDTH*0.50 - 8,y,key,UI_RIGHT|UI_SMALLFONT,color_red);
+		UI_DrawString(SCREEN_WIDTH*0.50 - 8,y,key,UI_RIGHT|UI_SMALLFONT,color_white); // BFP - modified color
 		UI_DrawString(SCREEN_WIDTH*0.50 + 8,y,value,UI_LEFT|UI_SMALLFONT,text_color_normal);
 
 		y += SMALLCHAR_HEIGHT;
@@ -198,38 +198,41 @@ void UI_ServerInfoMenu( void )
 	s_serverinfo.menu.wrapAround = qtrue;
 	s_serverinfo.menu.fullscreen = qtrue;
 
-	s_serverinfo.banner.generic.type  = MTYPE_BTEXT;
+	// BFP - Menu background
+	s_serverinfo.menubg.generic.type  = MTYPE_BITMAP;
+	s_serverinfo.menubg.generic.name  = ART_MENUBG;
+	s_serverinfo.menubg.generic.flags = QMF_LEFT_JUSTIFY | QMF_INACTIVE;
+	s_serverinfo.menubg.generic.x	  = 0;
+	s_serverinfo.menubg.generic.y	  = 0;
+	s_serverinfo.menubg.width		  = 640;
+	s_serverinfo.menubg.height		  = 480;
+
+	// BFP - barlog
+	s_serverinfo.barlog.generic.type  = MTYPE_BITMAP;
+	s_serverinfo.barlog.generic.name  = ART_BARLOG;
+	s_serverinfo.barlog.generic.flags = QMF_LEFT_JUSTIFY | QMF_INACTIVE;
+	s_serverinfo.barlog.generic.x	  = 140;
+	s_serverinfo.barlog.generic.y	  = 5;
+	s_serverinfo.barlog.width		  = 355;
+	s_serverinfo.barlog.height		  = 90;
+
+	s_serverinfo.banner.generic.type  = MTYPE_PTEXT; // BFP - modified SERVER INFO title type
+	s_serverinfo.banner.generic.flags = QMF_CENTER_JUSTIFY|QMF_INACTIVE; // BFP - modified SERVER INFO title flags
 	s_serverinfo.banner.generic.x	  = 320;
-	s_serverinfo.banner.generic.y	  = 16;
+	s_serverinfo.banner.generic.y	  = 45; // BFP - modified SERVER INFO title y position
 	s_serverinfo.banner.string		  = "SERVER INFO";
 	s_serverinfo.banner.color	      = color_white;
-	s_serverinfo.banner.style	      = UI_CENTER;
-
-	s_serverinfo.framel.generic.type  = MTYPE_BITMAP;
-	s_serverinfo.framel.generic.name  = SERVERINFO_FRAMEL;
-	s_serverinfo.framel.generic.flags = QMF_INACTIVE;
-	s_serverinfo.framel.generic.x	  = 0;  
-	s_serverinfo.framel.generic.y	  = 78;
-	s_serverinfo.framel.width  	      = 256;
-	s_serverinfo.framel.height  	  = 329;
-
-	s_serverinfo.framer.generic.type  = MTYPE_BITMAP;
-	s_serverinfo.framer.generic.name  = SERVERINFO_FRAMER;
-	s_serverinfo.framer.generic.flags = QMF_INACTIVE;
-	s_serverinfo.framer.generic.x	  = 376;
-	s_serverinfo.framer.generic.y	  = 76;
-	s_serverinfo.framer.width  	      = 256;
-	s_serverinfo.framer.height  	  = 334;
+	s_serverinfo.banner.style	      = UI_CENTER|UI_BIGFONT; // BFP - modified SERVER INFO title color
 
 	s_serverinfo.add.generic.type	  = MTYPE_PTEXT;
 	s_serverinfo.add.generic.flags    = QMF_CENTER_JUSTIFY|QMF_PULSEIFFOCUS;
 	s_serverinfo.add.generic.callback = ServerInfo_Event;
 	s_serverinfo.add.generic.id	      = ID_ADD;
 	s_serverinfo.add.generic.x		  = 320;
-	s_serverinfo.add.generic.y		  = 371;
+	s_serverinfo.add.generic.y		  = 400; // BFP - modified ADD TO FAVORITES button y position
 	s_serverinfo.add.string  		  = "ADD TO FAVORITES";
 	s_serverinfo.add.style  		  = UI_CENTER|UI_SMALLFONT;
-	s_serverinfo.add.color			  =	color_red;
+	s_serverinfo.add.color			  =	color_white; // BFP - modified ADD TO FAVORITES button color
 	if( trap_Cvar_VariableValue( "sv_running" ) ) {
 		s_serverinfo.add.generic.flags |= QMF_GRAYED;
 	}
@@ -240,9 +243,9 @@ void UI_ServerInfoMenu( void )
 	s_serverinfo.back.generic.callback = ServerInfo_Event;
 	s_serverinfo.back.generic.id	   = ID_BACK;
 	s_serverinfo.back.generic.x		   = 0;
-	s_serverinfo.back.generic.y		   = 480-64;
-	s_serverinfo.back.width  		   = 128;
-	s_serverinfo.back.height  		   = 64;
+	s_serverinfo.back.generic.y		   = 480-80; // BFP - modified BACK button y position
+	s_serverinfo.back.width  		   = 80; // BFP - modified BACK button width
+	s_serverinfo.back.height  		   = 80; // BFP - modified BACK button height
 	s_serverinfo.back.focuspic         = SERVERINFO_BACK1;
 
 	trap_GetConfigString( CS_SERVERINFO, s_serverinfo.info, MAX_INFO_STRING );
@@ -260,9 +263,10 @@ void UI_ServerInfoMenu( void )
 	if (s_serverinfo.numlines > 16)
 		s_serverinfo.numlines = 16;
 
+	Menu_AddItem( &s_serverinfo.menu, (void*) &s_serverinfo.barlog ); // BFP - barlog
+	// BFP - NOTE: That also happens in original BFP. Adding the background, hides completely the server info, it would be advisable to find an alternative or... remove that background hidding info issue (·_·)
+	Menu_AddItem( &s_serverinfo.menu, (void*) &s_serverinfo.menubg ); // BFP - Menu background
 	Menu_AddItem( &s_serverinfo.menu, (void*) &s_serverinfo.banner );
-	Menu_AddItem( &s_serverinfo.menu, (void*) &s_serverinfo.framel );
-	Menu_AddItem( &s_serverinfo.menu, (void*) &s_serverinfo.framer );
 	Menu_AddItem( &s_serverinfo.menu, (void*) &s_serverinfo.add );
 	Menu_AddItem( &s_serverinfo.menu, (void*) &s_serverinfo.back );
 
