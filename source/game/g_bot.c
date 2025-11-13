d4f410975f274d0012b3bd331dbb30cd2e5d235a
LegendaryGuard <rootuser999@gmail.com>
2025-11-13 22:48:34 +0100

    game: Apply Quake3e patch in g_bot.c

--- source/game/g_bot.c
+++ source/game/g_bot.c
@@ -131,11 +131,11 @@ static void G_LoadArenasFromFile( const char *filename ) {
 
 	len = trap_FS_FOpenFile( filename, &f, FS_READ );
 	if ( f == FS_INVALID_HANDLE ) {
-		trap_Printf( va( S_COLOR_RED "file not found: %s\n", filename ) );
+		trap_Print( va( S_COLOR_RED "file not found: %s\n", filename ) );
 		return;
 	}
 	if ( len >= MAX_ARENAS_TEXT ) {
-		trap_Printf( va( S_COLOR_RED "file too large: %s is %i, max allowed is %i", filename, len, MAX_ARENAS_TEXT ) );
+		trap_Print( va( S_COLOR_RED "file too large: %s is %i, max allowed is %i", filename, len, MAX_ARENAS_TEXT ) );
 		trap_FS_FCloseFile( f );
 		return;
 	}
@@ -181,7 +181,7 @@ static void G_LoadArenas( void ) {
 		strcat(filename, dirptr);
 		G_LoadArenasFromFile(filename);
 	}
-	trap_Printf( va( "%i arenas parsed\n", g_numArenas ) );
+	trap_Print( va( "%i arenas parsed\n", g_numArenas ) );
 	
 	for( n = 0; n < g_numArenas; n++ ) {
 		Info_SetValueForKey( g_arenaInfos[n], "num", va( "%i", n ) );
@@ -619,7 +619,7 @@ static void G_AddBot( const char *name, float skill, const char *team, int delay
 	Info_SetValueForKey( userinfo, "name", nm );
 
 	Info_SetValueForKey( userinfo, "rate", "25000" );
-	Info_SetValueForKey( userinfo, "snaps", "30" );
+	Info_SetValueForKey( userinfo, "snaps", va( "%i", sv_fps.integer ) );
 	Info_SetValueForKey( userinfo, "skill", va("%1.2f", skill) );
 
 	// BFP - No handicap
@@ -679,7 +679,7 @@ static void G_AddBot( const char *name, float skill, const char *team, int delay
 
 	s = Info_ValueForKey(botinfo, "aifile");
 	if (!*s ) {
-		trap_Printf( S_COLOR_RED "Error: bot has no aifile specified\n" );
+		trap_Print( S_COLOR_RED "Error: bot has no aifile specified\n" );
 		return;
 	}
 
@@ -743,7 +743,7 @@ void Svcmd_AddBot_f( void ) {
 	// name
 	trap_Argv( 1, name, sizeof( name ) );
 	if ( !name[0] ) {
-		trap_Printf( "Usage: Addbot <botname> [skill 1-5] [team] [msec delay] [altname]\n" );
+		trap_Print( "Usage: Addbot <botname> [skill 1-5] [team] [msec delay] [altname]\n" );
 		return;
 	}
 
@@ -781,7 +781,7 @@ void Svcmd_AddBot_f( void ) {
 	// go ahead and load the bot's media immediately
 	if ( level.time - level.startTime > 1000 &&
 		trap_Cvar_VariableIntegerValue( "cl_running" ) ) {
-		trap_SendServerCommand( -1, "loaddefered\n" );	// FIXME: spelled wrong, but not changing for demo
+		trap_SendServerCommand( -1, "loaddeferred\n" );	// FIXME: spelled wrong, but not changing for demo
 	}
 }
 
@@ -797,7 +797,7 @@ void Svcmd_BotList_f( void ) {
 	char model[MAX_QPATH];
 	char aifile[MAX_QPATH];
 
-	trap_Printf( S_COLOR_RED "name             model            aifile              funname\n" );
+	trap_Print( S_COLOR_RED "name             model            aifile              funname\n" );
 	for ( i = 0; i < g_numBots; i++ ) {
 		Q_strncpyz( name, Info_ValueForKey( g_botInfos[i], "name" ), sizeof( name ) );
 		if ( !*name ) {
@@ -815,7 +815,7 @@ void Svcmd_BotList_f( void ) {
 		if ( !*aifile ) {
 			strcpy( aifile, "bots/default_c.c" );
 		}
-		trap_Printf( va( "%-16s %-16s %-20s %-20s\n", name, model, aifile, funname ) );
+		trap_Print( va( "%-16s %-16s %-20s %-20s\n", name, model, aifile, funname ) );
 	}
 }
 
@@ -890,11 +890,11 @@ static void G_LoadBotsFromFile( const char *filename ) {
 
 	len = trap_FS_FOpenFile( filename, &f, FS_READ );
 	if ( f == FS_INVALID_HANDLE ) {
-		trap_Printf( va( S_COLOR_RED "file not found: %s\n", filename ) );
+		trap_Print( va( S_COLOR_RED "file not found: %s\n", filename ) );
 		return;
 	}
 	if ( len >= MAX_BOTS_TEXT ) {
-		trap_Printf( va( S_COLOR_RED "file too large: %s is %i, max allowed is %i\n", filename, len, MAX_BOTS_TEXT ) );
+		trap_Print( va( S_COLOR_RED "file too large: %s is %i, max allowed is %i\n", filename, len, MAX_BOTS_TEXT ) );
 		trap_FS_FCloseFile( f );
 		return;
 	}
@@ -944,7 +944,7 @@ static void G_LoadBots( void ) {
 		strcat(filename, dirptr);
 		G_LoadBotsFromFile(filename);
 	}
-	trap_Printf( va( "%i bots parsed\n", g_numBots ) );
+	trap_Print( va( "%i bots parsed\n", g_numBots ) );
 }
 
 
@@ -956,7 +956,7 @@ G_GetBotInfoByNumber
 */
 char *G_GetBotInfoByNumber( int num ) {
 	if( num < 0 || num >= g_numBots ) {
-		trap_Printf( va( S_COLOR_RED "Invalid bot number: %i\n", num ) );
+		trap_Print( va( S_COLOR_RED "Invalid bot number: %i\n", num ) );
 		return NULL;
 	}
 	return g_botInfos[num];
