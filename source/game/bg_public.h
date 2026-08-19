1c52e39cfbfe02ea67c379d3af16f645abb39989
LegendaryGuard <rootuser999@gmail.com>
2026-08-20 01:31:35 +0200

    game: Move patched Q3e custom functions from q_shared.h to bg_public.h

--- source/game/bg_public.h
+++ source/game/bg_public.h
@@ -832,3 +832,26 @@ qboolean	BG_PlayerTouchesItem( playerState_t *ps, entityState_t *item, int atTim
 #define TMOD_075	2292106
 #define TMOD_1000	5730265
 #define TMOD_2000	5730265
+
+
+// custom functions
+int BG_sprintf( char *buf, const char *format, ... );
+int Q_vsprintf( char *buffer, const char *fmt, va_list argptr );
+
+char *Q_stristr( const char * str1, const char * str2 );
+
+char *strtok( char *strToken, const char *strDelimit );
+char *EncodedString( const char *str );
+char *DecodedString( const char *str );
+
+void BG_CleanName( const char *in, char *out, int outSize, const char *blankString );
+char *BG_StripColor( char *string );
+
+void Q_strcpy( char *dst, const char *src );
+char *Q_stradd( char *dst, const char *src );
+int Q_sscanf( const char *buffer, const char *fmt, ... );
+
+int replace_s( char * str1, char * str2, char * src, int max_len );
+qboolean replace1( const char match, const char replace, char *str );
+
+qboolean  BigEndian( void );
