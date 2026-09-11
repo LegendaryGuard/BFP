c8a3851e781d98db4dd73a62b53102076d824494
LegendaryGuard <rootuser999@gmail.com>
2026-09-12 00:08:03 +0200

    cgame - game: Add ExitLevel function in the header and remove unused variable in map reveal

--- source/game/g_local.h
+++ source/game/g_local.h
@@ -779,6 +779,7 @@ void FindIntermissionPoint( void );
 void SetLeader(int team, int client);
 void CheckTeamLeader( int team );
 void G_RunThink (gentity_t *ent);
+void ExitLevel( void );
 void QDECL G_LogPrintf( const char *fmt, ... );
 void SendScoreboardMessageToAllClients( void );
 void QDECL G_Printf( const char *fmt, ... );
